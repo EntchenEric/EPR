8fe5f1cc617f1a32de28a5fa191b79149db51864
FxnnZz <148431585+FxnnZz@users.noreply.github.com>
2023-11-23 10:32:39 +0100

    SquareRootFInn
    EPR Aufgbe SquareRoot

---
+++ src/main/java/squareroot/SquareRootFinn.java
@@ -0,0 +1,16 @@
+package squareroot;
+
+public class SquareRootFinn {
+
+  public static boolean isSquareRoot(int base, long radical) {
+    
+    return (long) base * base == radical;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(isSquareRoot(5, 25));
+  }
+
+
+}
