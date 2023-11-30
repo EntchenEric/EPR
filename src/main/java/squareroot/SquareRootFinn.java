c22519cb1ca6666c5c18e5ea5595a8c6afb88b96
FxnnZz <148431585+FxnnZz@users.noreply.github.com>
2023-11-30 10:29:04 +0100

    aufgabe lösung
    lösung so

--- src/main/java/squareroot/SquareRootFinn.java
+++ src/main/java/squareroot/SquareRootFinn.java
@@ -3,13 +3,15 @@ package squareroot;
 public class SquareRootFinn {
 
   public static boolean isSquareRoot(int base, long radical) {
-    
-    return (long) base * base == radical;
+
+    return ((long) base * base == radical) && (base >= 0);
   }
+
 
   public static void main(String[] args) {
 
     System.out.println(isSquareRoot(5, 25));
+    System.out.println(isSquareRoot(-5,25));
   }
 
 
