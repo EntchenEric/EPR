69f980ae7667a70f096e7be0d15fe1d568201ef5
Maurice Monath <maurice@monath.xyz>
2023-12-04 21:20:19 +0100

    do task DigitsMaurice

--- src/main/java/digit/DigitsMaurice.java
+++ src/main/java/digit/DigitsMaurice.java
@@ -1,23 +1,11 @@
 package digit;
+
 public class DigitsMaurice {
-  static long productOfDigits(int number) {
-    // Initialisiere das Produkt mit 1
-    int product = 1;
+  static long productOfDigits(int a) {
+    return (a <= 10)
+           ? a
+           : productOfDigits(a / 10) + (a % 10);
 
-    // Extrahiere jede Ziffer und multipliziere sie mit dem Produkt
-    while (number != 0) {
-      int digit = number % 10;
-
-      // Falls die Ziffer nicht null ist, multipliziere sie mit dem Produkt
-      if (digit != 0) {
-        product *= digit;
-      }
-
-      // Teile die Zahl durch 10, um die nächste Ziffer zu erhalten
-      number /= 10;
-    }
-
-    return product;
   }
 //
 //  static boolean containsDigit(long, int) {
@@ -30,8 +18,8 @@ public class DigitsMaurice {
 
   public static void main(String[] args) {
     System.out.println(productOfDigits(4));      // Ausgabe: 4
-    System.out.println(productOfDigits(0));      // Ausgabe: 0
-    System.out.println(productOfDigits(5201));   // Ausgabe: 0
-    System.out.println(productOfDigits(12345));  // Ausgabe: 120
+   System.out.println(productOfDigits(0));      // Ausgabe: 0
+  System.out.println(productOfDigits(5201));   // Ausgabe: 0
+   System.out.println(productOfDigits(12345));  // Ausgabe: 120
   }
 }
