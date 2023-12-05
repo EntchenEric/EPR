40904d966593aea17359b33a8939853d8b682fb2
Maurice Monath <maurice@monath.xyz>
2023-12-05 13:42:11 +0100

    Do Task DistinctDigitsMaurice

--- src/main/java/digit/DistinctDigitsMaurice.java
+++ src/main/java/digit/DistinctDigitsMaurice.java
@@ -1,4 +1,18 @@
 package digit;
 
+/**
+ * The OrderDigitsMaurice class provides methods for checking the ascending order of digits in a number
+ * and inserting a digit into a number while maintaining descending order of digits.
+ */
 public class DistinctDigitsMaurice {
+
+  public static int countDistinctDigits(int number) {
+    return (number < 10)
+           ? 1
+           : 1 + countDistinctDigits(number / 10);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(countDistinctDigits(10));
+  }
 }
