fc9387298469ea0e1ef804fd265a45699742fab3
EntchenEric <eseidel2004@gmail.com>
2023-12-07 10:29:36 +0100

    Worked on Fractions

--- src/main/java/fraction/FractionEric.java
+++ src/main/java/fraction/FractionEric.java
@@ -1,7 +1,24 @@
 package fraction;
 
 public class FractionEric {
+    long calculatedFraction;
+    public FractionEric (long fraction) {
+        calculatedFraction = fraction / 1;
+    }
+
+    public FractionEric (long fraction1, long fraction2) {
+        calculatedFraction = fraction1 / fraction2;
+    }
+
+    public FractionEric add(FractionEric fraction){
+        return new FractionEric(fraction.calculatedFraction, calculatedFraction);
+    }
+
+    public String asText(){
+        return calculatedFraction.toString();
+    }
     public static void main(String[] args) {
 
     }
 }
+
