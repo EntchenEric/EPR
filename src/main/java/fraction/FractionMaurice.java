bd44650d95968c3b54a99c74802e76453b046a19
Maurice Monath <maurice@monath.xyz>
2023-12-07 10:30:42 +0100

    update task S

--- src/main/java/fraction/FractionMaurice.java
+++ src/main/java/fraction/FractionMaurice.java
@@ -2,8 +2,20 @@ package fraction;
 
 public class FractionMaurice {
 
+  public FractionMaurice(long fraction) {
 
+    fraction = fraction / 1;
+
+  }
+
+  public FractionMaurice(long numerator, long denominator) {
+
+  }
+
+  public double add(FractionMaurice fraction){
+
+  }
   public static void main(String[] args) {
-
+    new FractionMaurice(2);
   }
 }
