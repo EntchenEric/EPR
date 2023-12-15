fdf38e6c9b07a4a1138b501fa8a1fb654942bce8
EntchenEric <eseidel2004@gmail.com>
2023-12-15 09:16:45 +0100

    Added documentation.

--- src/main/java/repeatstring/RepeatStringEric.java
+++ src/main/java/repeatstring/RepeatStringEric.java
@@ -1,6 +1,15 @@
 package repeatstring;
 
+/**
+ * Repeats a String n times.
+ */
 public class RepeatStringEric {
+  /**
+   * Repeats a String s n times.
+   * @param s the String you want to repeat
+   * @param n how often you want to repeat the String,
+   * @return the String repeated n times.
+   */
   public static String repeat(String s, int n){
     String str = "";
     for (int i = 0; i < n; i++) {
@@ -9,6 +18,13 @@ public class RepeatStringEric {
     return str;
   }
 
+  /**
+   * Repeats a String s n times and adds a string between the strings.
+   * @param s1 The string you want to repeat
+   * @param n how often you want to repeat the String.
+   * @param s2 the String Seperator.
+   * @return the String repeated n times.
+   */
   public static String repeat(String s1, int n, String s2){
     String str = "";
     for (int i = 0; i < n; i++) {
@@ -18,6 +34,10 @@ public class RepeatStringEric {
     return str;
   }
 
+  /**
+   * the main method of the class.
+   * @param args not used.
+   */
   public static void main(String[] args){
     System.out.println(repeat("wau", 2));
     System.out.println(repeat("wau", 2, "--"));
