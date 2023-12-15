65aa717c4ba4b2ae5ee3010a0c03131022bf9e01
EntchenEric <eseidel2004@gmail.com>
2023-12-15 08:50:45 +0100

    Added documentation

--- src/main/java/fraction/FractionEric.java
+++ src/main/java/fraction/FractionEric.java
@@ -4,16 +4,30 @@ public class FractionEric {
   long numerator;
   long denominator;
 
+  /**
+   * Creates a new Fraction with the given numerator and denominator.
+   * @param numerator The numerator of the new Fraction.
+   */
   public FractionEric(long numerator) {
     this.numerator = numerator;
     this.denominator = 1;
   }
 
+  /**
+   * Creates a new Fraction with the given numerator and denominator.
+   * @param numerator The numerator of the new Fraction.
+   * @param denominator The denominator of the new Fraction.
+   */
   public FractionEric(long numerator, long denominator) {
     this.numerator = numerator;
     this.denominator = denominator;
   }
 
+  /**
+   * Adds the given Fraction to this Fraction and returns the result as a new Fraction.
+   * @param fraction The Fraction to add to this Fraction.
+   * @return The result of the addition.
+   */
   public FractionEric add(FractionEric fraction) {
     shorten();
     fraction.shorten();
@@ -25,23 +39,40 @@ public class FractionEric {
     return newFraction;
   }
 
+  /**
+   * Returns the textual representation of this Fraction.
+   * @return The textual representation of this Fraction.
+   */
   public String asText() {
     shorten();
     return this.numerator + "/" + this.denominator;
   }
 
+  /**
+   * Shortens the Fraction.
+   */
   public void shorten(){
     long ggT = ggT(this.numerator, this.denominator);
     this.numerator = this.numerator / ggT;
     this.denominator = this.denominator / ggT;
   }
 
+  /**
+   * Calculates the greatest common divisor of the given numbers.
+   * @param m The first number.
+   * @param n The second number.
+   * @return The greatest common divisor of the given numbers.
+   */
   public static long ggT(long m, long n) {
     return n == 0
             ? m
             : ggT(n, m % n);
   }
 
+  /**
+   * Main method. Only used for debugging purposes.
+   * @param args Command line arguments.
+   */
   public static void main(String[] args){
     FractionEric fraction = new FractionEric(4, 1);
     FractionEric fraction2 = new FractionEric(8, 2);
