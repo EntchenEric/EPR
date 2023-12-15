edae3c649bdbc0bb09f6a7492ca4b981d51e9a0f
EntchenEric <eseidel2004@gmail.com>
2023-12-15 09:14:00 +0100

    Changed descending digits so it works as intended.

--- src/main/java/digit/DescendingDigitsEric.java
+++ src/main/java/digit/DescendingDigitsEric.java
@@ -1,13 +1,14 @@
 package digit;
 
+
 public class DescendingDigitsEric {
   public static boolean isDescending(int n){
     String nAsString = String.valueOf(n);
-    int lastDigit = 0;
+    int lastDigit = 10;
     for (int i = 0; i < nAsString.length(); i++) {
       String chari = String.valueOf(nAsString.charAt(i));
       int digit = Integer.parseInt(chari);
-      if(digit < lastDigit){
+      if(digit >= lastDigit){
         return false;
       }
       lastDigit = digit;
@@ -19,6 +20,7 @@ public class DescendingDigitsEric {
     System.out.println(isDescending(12345));
     System.out.println(isDescending(123456789));
     System.out.println(isDescending(1234567898));
+    System.out.println(isDescending(987654321));
 
   }
 }
