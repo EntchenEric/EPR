4a5dc9fddaec9a1b12f890f7abd2cf210f36edfe
EntchenEric <eseidel2004@gmail.com>
2023-12-15 09:24:58 +0100

    Started working on Fibonacci

---
+++ src/main/java/fibonacci/FibonacciEric.java
@@ -0,0 +1,13 @@
+package fibonacci;
+
+public class FibonacciEric {
+  public static long fib(int n){
+    int fibo = 0;
+    for (int i = 0; i < n; i++) {
+      if(i <= 1){
+        fibo = n;
+      }
+    }
+    return 0;
+  }
+}
