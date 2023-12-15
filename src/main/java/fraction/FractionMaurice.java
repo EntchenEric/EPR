36da23509c01b604fd03c08c5d73d5d9e99d8a28
maurice-monath <maurice@monath.xyz>
2023-12-15 08:40:34 +0100

    fix error

--- src/main/java/fraction/FractionMaurice.java
+++ src/main/java/fraction/FractionMaurice.java
@@ -1,34 +1,26 @@
 package fraction;
 
 public class FractionMaurice {
-  long numerator;
-  long denumerator;
-
-  public FractionMaurice(long fraction) {
+  long calculatedFraction;
+  public FractionMaurice (long fraction) {
     calculatedFraction = fraction / 1;
   }
 
-  public FractionMaurice(long fraction1, long fraction2) {
-    numerator = fraction1;
-    denumerator = fraction2;
+  public FractionMaurice (long fraction1, long fraction2) {
+    calculatedFraction = fraction1 / fraction2;
   }
 
-  public FractionMaurice add(FractionMaurice fraction) {
+  public FractionMaurice add(FractionMaurice fraction){
     return new FractionMaurice(fraction.calculatedFraction, calculatedFraction);
   }
 
-  public String asText() {
+  public String asText(){
     return "calculatedFraction";
   }
-
-  public static int ggt(int m, int n) {
+  public static int ggT(int m, int n){
     return n == 0
-           ? m
-           : ggt(n, m % n);
-  }
-
-  public static void main(String[] args) {
-
+            ? m
+            : ggT(n, m%n);
   }
 }
 
